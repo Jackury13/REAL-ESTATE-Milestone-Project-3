0723513efecec71f8f257679db5b515686a706eb
Carlos Sosa <isek7or@gmail.com>
2024-04-22 20:45:35 -0700

    loading client

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
-const express = require("express");
+const express = require('express');
+// const mongoose = require('mongoose')
+// const dotenv = require('dotenv').config()
+// const cors = require('cors')
 
 const app = express();
 
