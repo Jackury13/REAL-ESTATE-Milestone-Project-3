87602a34e8cd2ecd1a943f8e4b22c2e8a6cf2072
Carlos Sosa <isek7or@gmail.com>
2024-04-22 22:28:32 -0700

    linked back front and DB

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-// const mongoose = require('mongoose')
-// const dotenv = require('dotenv').config()
-// const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello to Real Estate API");
 })
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("Server has been started"));
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+const PORT = process.env.PORT
