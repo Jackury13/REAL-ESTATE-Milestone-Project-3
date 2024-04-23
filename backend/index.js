ed5524245a6597dc6bd2be4e832046ddd9779870
Carlos Sosa <isek7or@gmail.com>
2024-04-23 01:41:14 -0700

    updated index in BE and created fetchAPI in FE

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,32 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const app = express()
-const userController = require('./controllers/usercontroller')
-const propertyController = require('./controllers/propertyController')
-const authController = require('./controllers/authController')
-const commentController = require('./controllers/commentController')
-const yachtController = require('./controllers/yachtController')
-const uploadController = require('./controllers/uploadController')
+// const userController = require('./controllers/usercontroller')
+// const authController = require('./controllers/authController')
+// const commentController = require('./controllers/commentController')
+// const propertyController = require('./controllers/propertyController')
+// const uploadController = require('./controllers/uploadController')
+// const userController = require('./controllers/usercontroller')
+// const yachtController = require('./controllers/yachtController')
+
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use('/images', express.static("public/images"))
+
+// app.use('/auth', authController)
+// app.use('/comment', commentController)
+// app.use('/property', propertyController)
+// app.use('/upload', uploadController)
+// app.use('/user', userController)
+// app.use('/yacht', yachtController)
 
 app.get("/", (req, res) => {
     res.send("Hello to Real Estate API")
 });
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
---
+++ client/src/util/fetchAPI.js
@@ -0,0 +1,22 @@
+const BASE_URL = "http://localhost:5000";
+
+export const request = async () => {
+    let res
+    let data
+    switch () {
+        case "GET":
+            return data
+
+        case "POST":
+            return data
+
+        case "PUT":
+            return data
+
+        case "DELETE":
+            return data
+
+        default:
+            return
+    }
+}
