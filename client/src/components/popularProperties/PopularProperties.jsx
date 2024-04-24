ceb3c1783a7328bc353efc6a2b6bbf1065787b1d
pittman.anthony17@gmail.com <pittman.anthony17@gmail.com>
2024-04-23 21:01:01 -0700

    estate and yacht pictures

--- client/src/components/popularProperties/PopularProperties.jsx
+++ client/src/components/popularProperties/PopularProperties.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import classes from './popularProperties.module.css'
+import { Link } from 'react-router-dom'
 
 
 const PopularProperties = () => {
   return (
-    <div>PopularProperties</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.titles}>
+          <h5>Different types of properties</h5>
+          <h2>Best type of properties for you </h2>
+        </div>
+        <div className={classes.properties}>
+          <Link to={'/properties?type=beach&continent=0&priceRange=1'}>
+            <img src={img1} />
+            <div className={classes.quantity}>34 properties</div>
+            <h5>Beach properties</h5>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
