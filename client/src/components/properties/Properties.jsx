deae3dc1b60a9ee72166f7ca1d255a517b3cf68c
pittman.anthony17@gmail.com <pittman.anthony17@gmail.com>
2024-04-25 19:53:26 -0700

    properties updated

--- client/src/components/properties/Properties.jsx
+++ client/src/components/properties/Properties.jsx
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { useState } from 'react'
+import { arrPriceRanges } from '../../util/idxToPriceRange'
 import classes from './properties.module.css'
-import { useLocation } from 'react-router-dom'
-
+import { useEffect } from 'react'
+import { continentToIdx } from '../../util/idxToContinent'
+import { request } from '../../util/fetchAPI'
+import PropertyCard from '../propertyCard/PropertyCard'
 
 const Properties = () => {
   const [allProperties, setAllProperties] = useState([])
@@ -11,13 +17,16 @@ const Properties = () => {
   const arrQuery = query.split("&")
   const navigate = useNavigate()
 
-
- 
+  const handleState = (e) => {
+    setState(prev => {
+      return { ...prev, [e.target.name]: e.target.value }
+    })
+  }
   // fetch all properties
 
   useEffect(() => {
     const fetchAllProperties =async() => {
-    const data = await request('/property/getAll', 'GET')
+    const data = await request(`/property/getAll`, 'GET')
     setAllProperties(data)
     }
     fetchAllProperties()
@@ -53,11 +62,70 @@ const Properties = () => {
 
     const filteredProperties = allProperties.filter((property) => {
       const priceRange = arrPriceRanges[options.priceRange]
+      const minPrice = Number(priceRange.split('-')[0])
+      const maxPrice = Number(priceRange.split('-')[1])
+      const continent = continentToIdx(property.continent)
+      
+      if(
+        property.type === options.type
+        && continent === Number(options.continent)
+        && property.price >= minPrice && property.price <= maxPrice 
+      ){
+        return property
+      }
     })
+
+    const queryStr = `type=${options.type}&continent=${options.continent}&priceRange=${options.priceRange}`
+
+    navigate(` /properties?${queryStr}`, {replace: true})
+    setFilteredProperties(filteredProperties)
   }
 
   return (
-    <div>Properties</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.options}>
+          <select value={state?.type} name="type" onChange={handleState}>
+            <option disabled>Select type</option>
+            <option value="beach">Beach</option>
+            <option value="mountain">Mountain</option>
+            <option value="village">Village</option>
+          </select>
+          <select value={state?.priceRange} name="priceRange" onChange={handleState}>
+            <option disabled>Select Price Range</option>
+            <option value="0">0-100,000</option>
+            <option value="1">100,000-200,000</option>
+            <option value="2">200,000-300,000</option>
+            <option value="3">300,000-400,000</option>
+            <option value="4">400,000-500,000</option>
+          </select>
+          <select value={state?.continent} name="continent" onChange={handleState}>
+            <option disabled>Select Continent</option>
+            <option value="0">Europe</option>
+            <option value="1">Asia</option>
+            <option value="2">Africa</option>
+            <option value="3">South America</option>
+            <option value="4">North America</option>
+            <option value="5">Oceania</option>
+          </select>
+          <button className={classes.searchBtn}>
+            <AiOutlineSearch className={classes.searchIcon} onClick={handleSearch} />
+          </button>
+        </div>
+        {filteredProperties?.length > 0 ?
+          <>
+            <div className={classes.titles}>
+              <h5>Selected properties</h5>
+              <h2>Property you may like</h2>
+            </div>
+            <div className={classes.properties}>
+              {filteredProperties.map((property) => (
+                <PropertyCard key={property._id} property={property}/>
+              ))}
+            </div>
+          </> : <h2 className={classes.noProperty}>We have no properties with the specified options.</h2>}
+      </div>
+    </div>
   )
 }
 
