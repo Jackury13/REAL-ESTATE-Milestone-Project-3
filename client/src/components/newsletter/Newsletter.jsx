35c1749bd6ffe4b31e8ff36734e5c200f8947564
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2024-04-25 20:50:17 -0700

    news letter email sign up added

--- client/src/components/newsletter/Newsletter.jsx
+++ client/src/components/newsletter/Newsletter.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import classes from './newsletter.module.css'
 
-
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.titles}>
+          <h5>Want to stay up too date with our latest destinations?</h5>
+          <h2>Send us your email, we will do the rest. </h2>
+        </div>
+        <div className={classes.inputContainer}>
+          <input type="email" placeholder="Email" className={classes.input}/>
+          <button className={classes.button}>Send</button> 
+        </div>
+      </div>
+    </div>
   )
 }
 
