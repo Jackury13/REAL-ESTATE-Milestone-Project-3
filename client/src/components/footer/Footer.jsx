01f6ee9778ac5b873b6ce6504a091ca5c9f1658b
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2024-04-28 06:53:57 -0700

    created footer content and css styling. corrected classnames for PopularProperties.jsx

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,7 +4,24 @@ import classes from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className={classes.wrapper}>
+        <div className={classes.col}>
+          <h2> About Luxury State</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi obcaecati asperiores, dignissimos nisi nostrum accusantium ad ut totam at voluptatibus?</p>
+        </div>
+        <div className={classes.col}>
+          <h2> Contact Us</h2>
+          <span>Phone: (555)867-5309</span>
+          <span>Email: Luxurystate@luxury.com</span>
+        </div>
+        <div className={classes.col}>
+          <h2> Location</h2>
+          <span>777 Las Vegas Blvd</span>
+          <span>Las Vegas nv</span>
+        </div>
+      </div>
+    </footer>
   )
 }
 
