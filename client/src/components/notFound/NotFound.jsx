f4bb0e113f5e1db13e0ecbedc29afc28393ff8f3
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2024-04-29 03:03:52 -0700

    not found page created

--- client/src/components/notFound/NotFound.jsx
+++ client/src/components/notFound/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import classes from './notFound.module.css'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>Footer</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <h2>This page does not exist</h2>
+        <Link to='/'>Go back to home</Link>
+      </div>
+    </div>
   )
 }
 
